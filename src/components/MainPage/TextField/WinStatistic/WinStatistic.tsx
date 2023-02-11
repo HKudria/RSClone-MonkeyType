80f3edf056da34e33d9af628fffd12561a881702
Herman Kudria <h.kudrya@hotmail.com>
2023-02-11 16:39:54 +0100

    feat: user statistic page

--- src/components/MainPage/TextField/WinStatistic/WinStatistic.tsx
+++ src/components/MainPage/TextField/WinStatistic/WinStatistic.tsx
@@ -1,108 +1,105 @@
-import { FC, useState } from 'react';
+import {useState} from 'react';
 
-// import closeItem from '../../../../assets/image/closeItem.png';
 import s from './WinStatistic.module.css';
-import { MyResponsivePie } from './Diagram/Diagram';
+import {MyResponsivePie} from './Diagram/Diagram';
 
-
-
-export const WinStatistic: 
-  FC<{ startTime: number | null; 
-    endTime: number | null; 
-    length: number; 
-    errorChar: number; 
-    correctChar: number; 
-    text: string; 
+interface IWinStatisticProps {
+    startTime: number | null;
+    endTime: number | null;
+    length: number;
+    errorChar: number;
+    correctChar: number;
+    text: string;
     currIndex: number;
-    time: number | null | undefined;}> = 
-    ({startTime, endTime, length, errorChar, correctChar, text, currIndex, time}): JSX.Element => {
-  
-  const [isActiveCloseItem, setIsActiveCloseItem] = useState<boolean>(true);
+    time: number | null | undefined;
+    isUserPage?: boolean
+    closeModal?: () => void
+}
 
-  // const closeItemHandler = () => {
-  //   setIsActiveCloseItem(false);
-  // }
+export const WinStatistic = ({
+                                 startTime, endTime, length, errorChar, correctChar,
+                                 text, currIndex, time, isUserPage, closeModal
+                             }: IWinStatisticProps) => {
 
-  const countCorrectChar = () => {
-    if (length && errorChar) {
-      return length - errorChar;
-    } else return length;
-  }
+    const [isActiveCloseItem, setIsActiveCloseItem] = useState<boolean>(true);
 
-  const countFinishTime = () => {
-    if (endTime && startTime) {
-      const finishTime = ((endTime - startTime) / 1000).toFixed(2);
-      return `Finish time: ${finishTime} sec`;
-    } else return 'Finish time: No';
-  }
-
-  const countPercentOfCorrectTyping = () => {
-    if (correctChar && length) {
-      return Math.trunc((correctChar / length) * 100);
-    } else return '0';
-  }
-
-  const countPercentOfUnwrittenText = () => {
-    if (currIndex && length) {
-      return Math.trunc(((length - currIndex - 1) / length) * 100);
+    const countCorrectChar = () => {
+        if (length && errorChar) {
+            return length - errorChar;
+        } else return length;
     }
-  }
 
-  const showTimer = () => {
-    if (time) {
-      return `Timer: ${time} sec`;
-    } else return 'Timer: No timer';
-  }
+    const countFinishTime = () => {
+        if (endTime && startTime) {
+            const finishTime = ((endTime - startTime) / 1000).toFixed(2);
+            return `Finish time: ${finishTime} sec`;
+        } else return 'Finish time: No';
+    }
 
-  const playAgain = () => {
-    window.location.reload();
-  }
+    const countPercentOfCorrectTyping = () => {
+        if (correctChar && length) {
+            return Math.trunc((correctChar / length) * 100);
+        } else return '0';
+    }
 
-  const errorEndCorrectChars = [
-    {
-      'id': 'Correct chars',
-      'label': 'Correct chars',
-      'value': countCorrectChar(),
-      'color': 'hsl(167, 70%, 50%)'
-    },
-    {
-      'id': 'Incorrect chars',
-      'label': 'Incorrect Chars',
-      'value': errorChar,
-      'color': 'hsl(205, 70%, 50%)'
-    },
-  ]
+    const countPercentOfUnwrittenText = () => {
+        if (currIndex && length) {
+            return Math.trunc(((length - currIndex - 1) / length) * 100);
+        }
+    }
 
-  return (
-     <div className={isActiveCloseItem ? s.wrapper : s.wrapper_not_active}>
-        {/* <div className={s.closeContainer}>
-          <img src={closeItem}
-          alt='closeItem'
-          className={ s.closeItem}
-          onClick={closeItemHandler}
-            />
-        </div> */}
+    const showTimer = () => {
+        if (time) {
+            return `Timer: ${time} sec`;
+        } else return 'Timer: No timer';
+    }
 
-        <div className={s.winTable}>
-        <h2 className={s.title}>Game end statistics</h2>
-          
-            <div className={s.text}>Text: <span className={s.phrase}>{text}</span></div>
-            <div className={s.statistics}>
-              <MyResponsivePie data={errorEndCorrectChars} />
-              <div className={s.container}>
-                <div>
-                  <div className={s.item}>{countFinishTime()}</div>
-                  <div className={s.item}>Success rate: {countPercentOfCorrectTyping()} %</div>
+    const playAgain = () => {
+        window.location.reload();
+    }
+
+    const errorEndCorrectChars = [
+        {
+            'id': 'Correct chars',
+            'label': 'Correct chars',
+            'value': countCorrectChar(),
+            'color': 'hsl(167, 70%, 50%)'
+        },
+        {
+            'id': 'Incorrect chars',
+            'label': 'Incorrect Chars',
+            'value': errorChar,
+            'color': 'hsl(205, 70%, 50%)'
+        },
+    ]
+
+    return (
+        <div className={isActiveCloseItem ? s.wrapper : s.wrapper_not_active}>
+            <div className={s.winTable}>
+                <h2 className={s.title}>Game end statistics</h2>
+
+                <div className={s.text}>Text: <span className={s.phrase}>{text}</span></div>
+                <div className={s.statistics}>
+                    <MyResponsivePie data={errorEndCorrectChars}/>
+                    <div className={s.container}>
+                        <div>
+                            <div className={s.item}>{countFinishTime()}</div>
+                            <div className={s.item}>Success rate: {countPercentOfCorrectTyping()} %</div>
+                        </div>
+                        <div>
+                            <div className={s.item}>{showTimer()}</div>
+                            <div className={s.item}>Unwritten letters: {countPercentOfUnwrittenText()} %</div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                  <div className={s.item}>{showTimer()}</div>
-                  <div className={s.item}>Unwritten letters: {countPercentOfUnwrittenText()} %</div>
-                </div>
-              </div>
-          </div>
-          <button onClick={playAgain}
-           className={s.btn}>Try again</button>
+                {!isUserPage ?
+                    <button onClick={playAgain}
+                            className={s.btn}>Try again
+                    </button> : <button onClick={closeModal}
+                                        className={s.btn}>CloseModal
+                    </button>
+                }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
