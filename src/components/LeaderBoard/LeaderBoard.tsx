3b47c3b4144f21090ac3a56150aa602ebe6e3cfe
Herman Kudria <h.kudrya@hotmail.com>
2023-02-13 19:03:56 +0100

    feat: add pagination

--- src/components/LeaderBoard/LeaderBoard.tsx
+++ src/components/LeaderBoard/LeaderBoard.tsx
@@ -7,18 +7,39 @@ import {
     Table, TableBody,
     TableCell,
     TableContainer, TableHead,
-    TableRow
+    TableRow, TextField
 } from '@mui/material';
 import {IUserData} from '../Api/Interface';
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {API_URL} from '../Api/ApiHelper';
 import {UserStatistic} from '../UserStatistic/UserStatistic';
+import ReactPaginate from 'react-paginate';
+import s from './LeaderBoaerd.module.css'
 
 export const LeaderBoard = () => {
     const {t} = useTranslation('common');
-    const [usersData, setUsersData] = useState<IUserData[]>()
+    const [usersData, setUsersData] = useState<IUserData[]>([])
     const [errors, setErrors] = useState('');
     const [preloader, setPreloader] = useState(true);
+    const [itemOffset, setItemOffset] = useState(0);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
+    const endOffset = itemOffset + itemsPerPage;
+
+
+    const handlePageClick = (event: { selected: number; }) => {
+        const newOffset = (event.selected * itemsPerPage) % usersData.length;
+        setItemOffset(newOffset);
+    };
+
+    const onChangePageNumber = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const pages = parseInt(event.target.value)
+        if (pages >= 1) {
+            setItemsPerPage(pages)
+        } else {
+            setItemsPerPage(1)
+        }
+    }
+
 
     const parseUserData = () => {
         fetch(`${API_URL}/getLeaders`, {
@@ -45,22 +66,42 @@ export const LeaderBoard = () => {
         parseUserData()
     }, [])
 
+    const currentItems = usersData.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(usersData.length / itemsPerPage);
+
     return (
-        <>
-            {errors&&<Alert severity="error" sx={{mb: 2}}>{t(errors)}</Alert>}
+        <div className={s.container}>
+            {errors && <Alert severity="error" sx={{mb: 2}}>{t(errors)}</Alert>}
             {preloader ?
                 <CircularProgress/>
                 :
                 <Box sx={{
                     display: 'flex',
                     justifyContent: 'center',
+                    color: 'black',
                     background: 'white',
                     width: 'fit-content',
                     padding: 2,
                     flexDirection: 'column',
                     borderRadius: '25px'
                 }}>
-                    <h1>{t('user.title')}</h1>
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}>
+                        <h1>{t('statistic.leader')}</h1>
+                        <TextField
+                            id="filled-number"
+                            label={t('pagination.pages')}
+                            type="number"
+                            value={itemsPerPage}
+                            onChange={onChangePageNumber}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            variant="standard"
+                            sx={{maxWidth: '250px'}}
+                        /></Box>
                     {usersData &&
                         <TableContainer component={Paper}>
                             <Table sx={{minWidth: 700}} aria-label="customized table">
@@ -76,12 +117,31 @@ export const LeaderBoard = () => {
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {usersData.map((userData, index) => <UserStatistic gameInfo={userData} key={userData._id} isLeaders={true} index={index + 1}/>)}
+                                    {currentItems.map((userData, index) => <UserStatistic gameInfo={userData}
+                                                                                          key={userData._id}
+                                                                                          isLeaders={true}
+                                                                                          index={itemOffset + index + 1}/>)}
                                 </TableBody>
                             </Table>
                         </TableContainer>
                     }
+                    <ReactPaginate
+                        activeClassName={`${s.item} ${s.active}`}
+                        breakClassName={`${s.item} ${s.breakMe}`}
+                        breakLabel={'...'}
+                        nextLabel={t('pagination.next')}
+                        previousLabel={t('pagination.previous')}
+                        containerClassName={s.pagination}
+                        disabledClassName={s.disabledPage}
+                        marginPagesDisplayed={2}
+                        nextClassName={`${s.item} ${s.next}`}
+                        onPageChange={handlePageClick}
+                        pageCount={pageCount}
+                        pageClassName={`${s.item} ${s.paginationPage}`}
+                        pageRangeDisplayed={5}
+                        previousClassName={`${s.item} ${s.previous}`}
+                    />
                 </Box>}
-        </>
+        </div>
     );
 }
